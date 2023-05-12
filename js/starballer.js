18e8afb86accd13797a3c15dd4683eaa55660db2
Zach Dyer <zach.dyer@gmail.com>
2023-05-12 04:40:05 -0500

    navigation to 7 stations

--- js/starballer.js
+++ js/starballer.js
@@ -3,3 +3,14 @@ let hudTitle = document.querySelector('#hud-title')
 hudTitle.textContent = `Space Station ${spaceStationImage}`
 let pilotWindow = document.querySelector('#pilot-window')
 pilotWindow.style.backgroundImage = `url(../img/space-station-${spaceStationImage}.png)`
+let navigation = document.querySelector('#navigation')
+for(let i = 1; i <= 7; i++) {
+    const button = document.createElement("button");
+    button.setAttribute('class', 'btn btn-secondary')
+    button.textContent = `Space Station ${i}`;
+    button.addEventListener('click', ()=>{
+        pilotWindow.style.backgroundImage = `url(../img/space-station-${i}.png)`
+        hudTitle.textContent = `Space Station ${i}`
+    })
+    navigation.appendChild(button);
+}
